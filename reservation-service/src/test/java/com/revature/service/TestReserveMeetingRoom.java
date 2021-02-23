796e3ce73f07c1b3f291832f92c0f5a25b0bf159
Bryan.Wood <bryan.wood@lntinfotech.com>
2021-02-23 15:24:07 -0500

    started my card test reservemeetingroom
    built code stub

---
+++ reservation-service/src/test/java/com/revature/service/TestReserveMeetingRoom.java
@@ -0,0 +1,17 @@
+package com.revature.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestReserveMeetingRoom {
+
+	
+	@Test
+	public void contextLoads() {
+		
+	}
+
+}
