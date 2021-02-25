7603c2a6a3ca59e6be9edeac2f607cc2d2f48b3a
Bryan.Wood <bryan.wood@lntinfotech.com>
2021-02-25 17:11:09 -0500

    Update ReservationServiceImpl.java

--- reservation-service/src/main/java/com/revature/service/ReservationServiceImpl.java
+++ reservation-service/src/main/java/com/revature/service/ReservationServiceImpl.java
@@ -42,7 +42,7 @@ public class ReservationServiceImpl implements ReservationService {
     }
 
     @Override
-    public void assignBatch(Integer reservationId, Integer batchId) {
+    public void assignBatch(Integer batchId) {
 
     }
 
