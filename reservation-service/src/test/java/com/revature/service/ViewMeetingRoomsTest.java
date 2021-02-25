c256c7f71cb393380f63fd8dd8f81344495d5984
SavitaBhor <67609286+SavitaBhor@users.noreply.github.com>
2021-02-24 22:32:36 -0500

    Some changes test file

--- reservation-service/src/test/java/com/revature/service/ViewMeetingRoomsTest.java
+++ reservation-service/src/test/java/com/revature/service/ViewMeetingRoomsTest.java
@@ -1,5 +1,19 @@
 package com.revature.service;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class ViewMeetingRoomsTest {
+
+
+    @Before
+    public void preTest(){
+
+    }
+
+    @Test
+    public void test1(){
+
+    }
 
 }
