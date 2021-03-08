6fd416ce6017f0b093227e5e48f9cfc57c387260
TairovichJR <yokubov.omonjon@gmail.com>
2021-03-08 18:04:23 -0500

    swagger in progress

--- reservation-service/src/main/java/com/revature/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/revature/ReservationServiceApplication.java
@@ -3,11 +3,10 @@ package com.revature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ReservationServiceApplication {
-
 	public static void main( String[] args ) {
-		
 		SpringApplication.run( ReservationServiceApplication.class, args );
 	}
 }
