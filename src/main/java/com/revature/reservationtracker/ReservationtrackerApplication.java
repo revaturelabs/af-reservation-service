ad8768ec9337b8412c229b8bed494e8883515af3
Josh Barthelmess <joshbarthelmess3@gmail.com>
2021-03-30 17:46:26 -0400

    initial commit

---
+++ src/main/java/com/revature/reservationtracker/ReservationtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.revature.reservationtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationtrackerApplication.class, args);
+	}
+
+}
