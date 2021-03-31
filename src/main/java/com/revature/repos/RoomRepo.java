7e110a4f38f03f5214ad152e3b5f98686b9349e3
Josh Barthelmess <joshbarthelmess3@gmail.com>
2021-03-31 14:31:42 -0400

    tests complete

--- src/main/java/com/revature/repos/RoomRepo.java
+++ src/main/java/com/revature/repos/RoomRepo.java
@@ -1,6 +1,8 @@
 package com.revature.repos;
 
 import com.revature.entities.Reservation;
+import com.revature.entities.Room;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import java.util.Set;
 
 @Component
 @Repository
-public interface RoomRepo {
+public interface RoomRepo extends CrudRepository<Room, Integer> {
 }
--- src/test/java/com/revature/serviceTests/ReservationServiceTests.java
+++ src/test/java/com/revature/serviceTests/ReservationServiceTests.java
@@ -1,11 +1,11 @@
 package com.revature.serviceTests;
 
 import com.revature.entities.Reservation;
+import com.revature.entities.Room;
 import com.revature.entities.User;
 import com.revature.repos.ReservationRepo;
 import com.revature.repos.RoomRepo;
 import com.revature.services.ReservationService;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -51,6 +51,7 @@ public class ReservationServiceTests {
         long endTime = startTime + 3600;
         Reservation reservation = new Reservation(505,user.getEmail(),startTime,endTime,"jdjdjdjdjd",0);
 
+        Mockito.when(roomRepo.findById(0).orElse(null)).thenReturn(new Room());
         Reservation newReservation = service.createReservation(reservation);
         Assertions.assertEquals(0, newReservation.getReservationId());
         Assertions.assertEquals("reserved", newReservation.getStatus());
@@ -62,6 +63,17 @@ public class ReservationServiceTests {
         long endTime = startTime - 3600;
         Reservation reservation = new Reservation(505,user.getEmail(),startTime,endTime,"jdjdjdjdjd",0);
 
+        Mockito.when(roomRepo.findById(0).orElse(null)).thenReturn(new Room());
+        Assertions.assertThrows(ResponseStatusException.class, ()-> service.createReservation(reservation));
+    }
+
+    @Test
+    void create_reservation_invalid_room() {
+        long startTime = System.currentTimeMillis()/1000L;
+        long endTime = startTime - 3600;
+        Reservation reservation = new Reservation(505,user.getEmail(),startTime,endTime,"jdjdjdjdjd",1);
+
+        Mockito.when(roomRepo.findById(1).orElse(null)).thenReturn(null);
         Assertions.assertThrows(ResponseStatusException.class, ()-> service.createReservation(reservation));
     }
 
@@ -102,6 +114,17 @@ public class ReservationServiceTests {
     }
 
     @Test
+    void updated_reservation_time_not_found() {
+        long startTime = System.currentTimeMillis()/1000L;
+        long endTime = startTime + 3600;
+        Reservation reservation = new Reservation(1,user.getEmail(),startTime,endTime,"reserved",2);
+
+        Mockito.when(reservationRepo.findById(anyInt()).orElse(null)).thenReturn(null);
+
+        Assertions.assertThrows(ResponseStatusException.class, ()-> service.updateReservationTime(reservation));
+    }
+
+    @Test
     void cancel_reservation(){
         long startTime = System.currentTimeMillis()/1000L;
         long endTime = startTime + 3600;
@@ -112,5 +135,52 @@ public class ReservationServiceTests {
         Reservation cancelledReservation = service.cancelReservation(1);
 
         Assertions.assertEquals("cancelled", cancelledReservation.getStatus());
+    }
+
+    @Test
+    void get_reservation_by_id() {
+        long startTime = System.currentTimeMillis()/1000L;
+        long endTime = startTime + 3600;
+        Reservation reservation = new Reservation(1,user.getEmail(),startTime,endTime,"reserved",2);
+
+        Mockito.when(reservationRepo.findById(1).orElse(null)).thenReturn(reservation);
+
+        Reservation gotten = service.getReservationById(1);
+        Assertions.assertEquals(gotten.getStartTime(), reservation.getStartTime());
+        Assertions.assertEquals(gotten.getEndTime(), reservation.getEndTime());
+        Assertions.assertEquals(gotten.getRoomId(), reservation.getRoomId());
+    }
+
+    @Test
+    void get_reservation_by_id_not_found() {
+        Mockito.when(reservationRepo.findById(anyInt()).orElse(null)).thenReturn(null);
+        Assertions.assertThrows(ResponseStatusException.class, ()-> service.getReservationById(2));
+    }
+
+    @Test
+    void cancel_reservation_not_found() {
+        Mockito.when(reservationRepo.findById(anyInt()).orElse(null)).thenReturn(null);
+        Assertions.assertThrows(ResponseStatusException.class, ()-> service.cancelReservation(2));
+    }
+
+    @Test
+    void get_active_reservations_by_room_id() {
+        long startTime = System.currentTimeMillis()/1000L;
+        long endTime = startTime + 3600;
+        Set<Reservation> reservations = new HashSet<>();
+
+        Reservation reservation1 = new Reservation(1,user.getEmail(),startTime,endTime,"reserved",2);
+        Reservation reservation2 = new Reservation(2,user.getEmail(),startTime+3600,endTime+3600,"reserved",2);
+        Reservation reservation3 = new Reservation(3,user.getEmail(),startTime+7200,endTime+7200,"reserved",2);
+
+        reservations.add(reservation1);
+        reservations.add(reservation2);
+        reservations.add(reservation3);
+
+        Mockito.when(reservationRepo.findByRoomIdAndStatus(2, "reserved")).thenReturn(reservations);
+
+        Set<Reservation> returned = service.getActiveReservationsByRoomId(2);
+
+        Assertions.assertEquals(3, returned.size());
     }
 }
