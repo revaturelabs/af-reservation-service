b04cd1ac53d04c5494f95a8196fd6dac4f998da0
Josh Barthelmess <joshbarthelmess3@gmail.com>
2021-04-01 12:04:59 -0400

    finished repo tests, added test data to h2 database

--- src/test/java/com/revature/reservationtracker/repotests/ReservationRepoTest.java
+++ src/test/java/com/revature/reservationtracker/repotests/ReservationRepoTest.java
@@ -1,9 +1,7 @@
 package com.revature.reservationtracker.repotests;
 
 import com.revature.entities.Reservation;
-import com.revature.entities.Room;
 import com.revature.repos.ReservationRepo;
-import com.revature.repos.RoomRepo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,23 +11,68 @@ import java.util.Set;
 
 @SpringBootTest
 public class ReservationRepoTest {
-@Autowired
+    @Autowired
     ReservationRepo reservationRepo;
-@Autowired
-    RoomRepo roomRepo;
 
     @Test
-    public void find_By_RoomId(){
-       Set<Reservation> reservations= reservationRepo.findByRoomIdAndStatus(1,"reserved");
-        //System.out.println(roomRepo.findAll());
-        Assertions.assertNotEquals(0,reservations.size());
-
+    void find_by_room_id_and_status(){
+       Set<Reservation> reservations = reservationRepo.findByRoomIdAndStatus(1,"reserved");
+       Assertions.assertNotEquals(0,reservations.size());
+       for(Reservation r : reservations) {
+           Assertions.assertEquals("reserved", r.getStatus());
+           Assertions.assertEquals(1, r.getRoomId());
+       }
     }
 
     @Test
-    public void find_Conflict(){
-
-        
+    void find_by_room_id_and_status_cancelled() {
+        Set<Reservation> reservations = reservationRepo.findByRoomIdAndStatus(1, "cancelled");
+        Assertions.assertNotEquals(0, reservations.size());
+        for(Reservation r : reservations) {
+            Assertions.assertEquals(1, r.getRoomId());
+            Assertions.assertEquals("cancelled", r.getStatus());
+        }
     }
 
+    @Test
+    void find_conflicts_none(){
+        Set<Reservation> reservations = reservationRepo.findConflicts(2, 5000, 7000);
+        Assertions.assertEquals(0, reservations.size());
+    }
+
+    // in data base                                  |--------------------------|
+    // should find conflict for this          |-------------------|
+    @Test
+    void find_conflict_before_case() {
+        Set<Reservation> reservations = reservationRepo.findConflicts(1, 3650, 7250);
+        Assertions.assertNotEquals(0, reservations.size());
+        System.out.println(reservations);
+    }
+
+    // in data base                                  |--------------------------|
+    // should find conflict for this          |------------------------------------|
+    @Test
+    void find_conflicts_envelops_case() {
+        Set<Reservation> reservations = reservationRepo.findConflicts(2, 1000, 5000);
+        Assertions.assertNotEquals(0, reservations.size());
+        System.out.println(reservations);
+    }
+
+    // in data base                                  |--------------------------|
+    // should find conflict for this                            |------------------------|
+    @Test
+    void find_conflicts_after_case() {
+        Set<Reservation> reservations = reservationRepo.findConflicts(1, 5000, 8000);
+        Assertions.assertNotEquals(0, reservations.size());
+        System.out.println(reservations);
+    }
+
+    // in data base                                  |--------------------------|
+    // should find conflict for this                    |-----------------|
+    @Test
+    void find_conflicts_internal_case() {
+        Set<Reservation> reservations = reservationRepo.findConflicts(1, 4000, 6000);
+        Assertions.assertNotEquals(0, reservations.size());
+        System.out.println(reservations);
+    }
 }
