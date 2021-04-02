127ababece69921ab72a0e3e742f38bca80b1eb6
tom <xian.tom.wan@gmail.com>
2021-04-02 12:03:26 -0500

    security aspect update

--- src/main/java/com/revature/aspects/SecurityAspect.java
+++ src/main/java/com/revature/aspects/SecurityAspect.java
@@ -21,44 +21,51 @@ import javax.servlet.http.HttpServletResponse;
 public class SecurityAspect {
     private Logger logger = Logger.getLogger(SecurityAspect.class);
 
+    public static String getEnv(String key) {
+        return System.getenv(key);
+    }
+
     @Around("controllerMethodsPointCut()")
     public Object verifyJwt(ProceedingJoinPoint pjp) throws Throwable {
-//        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-//        HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
-//        String auth = request.getHeader("Authorization");
-//
-//        WebClient webClient = WebClient.create(System.getenv("AUTH_SERVER"));
-//        try{
-//            UserDTO userDTO = webClient
-//                    .post()
-//                    .body(Mono.just(auth), String.class)
-//                    .retrieve()
-//                    .onStatus(httpStatus -> HttpStatus.UNAUTHORIZED.equals(httpStatus),
-//                            clientResponse -> {
-//                                Mono.empty();
-//                                return null;
-//                            })
-//                    .bodyToMono(UserDTO.class).block();
-//            if (userDTO.getId() != 0){
-//                logger.info("JWT verified: " + userDTO);
-//                Object[] args = pjp.getArgs();
-//                args[0] = userDTO;
-//                Object obj = pjp.proceed();
-//                return obj;
-//            }
-//        }catch(Exception e){
-//            logger.error("Unable to verify JWT");
-//            response.sendError(401, "Unable to verify JWT");
-//        }
-//        return null;
+        // PRODUCTION CODE
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
+        String auth = request.getHeader("Authorization");
+
+        WebClient webClient = WebClient.create(this.getEnv("AUTH_SERVER"));
+        try{
+            logger.info("TEST TEST");
+            UserDTO userDTO = webClient
+                    .post().uri("/verify")
+                    .body(Mono.just(auth), String.class)
+                    .retrieve()
+                    .onStatus(httpStatus -> HttpStatus.UNAUTHORIZED.equals(httpStatus),
+                            clientResponse -> {
+                                Mono.empty();
+                                return null;
+                            })
+                    .bodyToMono(UserDTO.class).block();
+            logger.info(userDTO);
+            if (userDTO.getId() != 0){
+                logger.info("JWT verified: " + userDTO);
+                Object[] args = pjp.getArgs();
+                args[0] = userDTO;
+                Object obj = pjp.proceed();
+                return obj;
+            }
+        }catch(Exception e){
+            logger.error("Unable to verify JWT: " + e.getMessage());
+            response.sendError(401, "Unable to verify JWT");
+        }
+        return null;
 
         // THIS IS FOR TESTING (UNABLE TO FIGURE OUT HOW TO MOCK OUT ASPECT)
-        UserDTO userDTO = new UserDTO(1,"email@revature.com","trainer");
-        logger.info("JWT verified: " + userDTO);
-        Object[] args = pjp.getArgs();
-        args[0] = userDTO;
-        Object obj = pjp.proceed();
-        return obj;
+//        UserDTO userDTO = new UserDTO(1,"email@revature.com","trainer");
+//        logger.info("JWT verified: " + userDTO);
+//        Object[] args = pjp.getArgs();
+//        args[0] = userDTO;
+//        Object obj = pjp.proceed();
+//        return obj;
     }
 
     @Pointcut("@annotation(com.revature.aspects.Verify)")
